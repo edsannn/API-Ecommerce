85b63998159d03c30eeedcd6c66db58f0e2c6cb7
Edson Santos <cmo.edson@gmail.com>
2023-01-19 13:33:15 -0300

    Service layer, component registration

--- src/main/java/com/edsannn/course/repositories/UserRepository.java
+++ src/main/java/com/edsannn/course/repositories/UserRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edsannn.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{	
-	
-	
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
-
-
