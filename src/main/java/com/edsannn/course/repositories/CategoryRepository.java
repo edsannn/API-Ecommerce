6209cf48ded1c990af581024902b5acdc7b01baa
Edson Santos <cmo.edson@gmail.com>
2023-01-22 21:05:55 -0300

    Category

---
+++ src/main/java/com/edsannn/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.edsannn.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edsannn.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
